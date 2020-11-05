252082284a5612d4a4b2090c9314df0a3ecbe0c7
sarvanios6 <sarvanios6@gmail.com>
2020-11-06 01:25:37 +0530

    router updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {HashRouter as Router, Route} from "react-router-dom"
 import login from './component/Login/Login'
 // import home from './component/Home/Home'
 // import blogs from './component/Blogs/Blogs'
@@ -16,14 +16,12 @@ export const PATHS = {
 };
 const App = () => (
     <Router>
-      <Switch>
         <Route exact path={PATHS.DASHBOARD} component={login}/>
         <Route path={PATHS.LOGIN} component={login}/>
         {/*<Route exact path={PATHS.HOME} component={home}/>*/}
         {/*<Route exact path={PATHS.USERS} component={users}/>*/}
         {/*<Route exact path={PATHS.BLOGS} component={blogs}/>*/}
         {/*<Route exact path={PATHS.BLOGS_ID} component={blogsDetails}/>*/}
-      </Switch>
     </Router>
 )
 export default App
